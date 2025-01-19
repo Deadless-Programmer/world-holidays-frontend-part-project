75a78240c90a1c45e5c2b818647bb9d77a41cd0e
Deadless-Programmer <webtuhin@gmail.com>
2025-01-19 19:08:17 +0600

    home page working

---
+++ src/components/home/WhoAreWe/WhoAreWe.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const WhoAreWe = () => {
+  return (
+    <div> <section className="dark:bg-gray-100 dark:text-gray-800 mt-32 md:mt-0">
+    <div className="container max-w-6xl p-6 mx-auto space-y-6 sm:space-y-12" bis_skin_checked="1">
+      <div  className="block max-w-sm gap-3 mx-auto sm:max-w-full group hover:no-underline focus:no-underline lg:grid lg:grid-cols-12 dark:bg-gray-50">
+        <img src="https://i.postimg.cc/nV29vpYr/woman-6010055-1920.jpg" alt="" className="object-cover w-full h-64 rounded sm:h-96 lg:col-span-7 dark:bg-gray-500" />
+        <div className="p-6 space-y-2 lg:col-span-5" bis_skin_checked="1">
+            
+          <h3 className="text-2xl font-semibold sm:text-4xl ">Find Your Best Travel Details With Us</h3>
+          {/* <span className="text-xs dark:text-gray-600">February 19, 2021</span> */}
+          <p>Ei delenit sensibus liberavisse pri. Quod suscipit no nam. Est in graece fuisset, eos affert putent doctus id.</p>
+
+          <div className='bg-black h-[2px] '></div>
+        </div>
+      </div>
+      
+    </div>
+  </section> </div>
+  )
+}
+
+export default WhoAreWe
