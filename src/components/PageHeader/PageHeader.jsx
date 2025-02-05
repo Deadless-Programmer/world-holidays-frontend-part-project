92ead3d99a7a617fe5e4bb89396ecbe150de8bcf
Deadless-Programmer <webtuhin@gmail.com>
2025-02-05 13:31:40 +0600

    header component responsive done

--- src/components/PageHeader/PageHeader.jsx
+++ src/components/PageHeader/PageHeader.jsx
@@ -2,13 +2,13 @@ import React from 'react'
 
 const PageHeader = () => {
   return (
-    <div className='relative h-[60vh] bg-[url("https://cdn.pixabay.com/photo/2023/03/31/14/39/ocean-7890172_1280.jpg")] bg-cover bg-center'>
+    <div className='relative  h-[60vh] bg-[url("https://cdn.pixabay.com/photo/2023/03/31/14/39/ocean-7890172_1280.jpg")] bg-cover bg-center'>
     <div className="absolute inset-0 bg-slate-900 bg-opacity-50">
     
     </div>
     <div className="absolute inset-0 flex flex-col items-center justify-center">
       <p className="text-white text-xl font-bold font-nunito">About Us</p>
-      <h1 className='text-white text-5xl font-bold font-playfair mt-2'>Our Story In World Holidays </h1>
+      <h1 className='text-white text-3xl md:text-5xl font-bold font-playfair mt-2 text-center'>Our Story In World Holidays </h1>
     </div>
     
   </div>
