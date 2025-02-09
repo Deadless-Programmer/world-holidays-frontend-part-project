673355e17db27b45d74c95c0d6bc2d34b6ec57b6
Deadless-Programmer <webtuhin@gmail.com>
2025-02-09 11:23:40 +0600

    our service add

---
+++ src/components/Services/OurServices/OurServices.jsx
@@ -0,0 +1,62 @@
+import React from 'react'
+
+const OurServices = () => {
+  return (
+    <div><section className=" max-w-7xl mx-auto py-6 sm:py-12 dark:bg-gray-100 dark:text-gray-800">
+	<div className="container p-6 mx-auto space-y-8">
+		<div className="space-y-2 text-center">
+			<h2 className="text-3xl font-bold font-playfair"> <span className='text-orange-500'>Explore</span> Our Services</h2>
+			<p className=" text-sm dark:text-gray-600 font-nunito">We offer customized tours, hassle-free bookings, and everything you need for a seamless travel experience.</p>
+		</div>
+		<div className="grid grid-cols-1 gap-x-4 gap-y-8 md:grid-cols-2 lg:grid-cols-4">
+			<article className="flex flex-col dark:bg-gray-50">
+				<a rel="noopener noreferrer" href="#" aria-label="Te nulla oportere reprimique his dolorum">
+					<img alt="" className="object-cover w-full h-52 hover:scale-105 transition-all duration-1000 dark:bg-gray-500" src="https://images.pexels.com/photos/20392471/pexels-photo-20392471/free-photo-of-colosseum-in-rome-in-sunlight.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" />
+				</a>
+				<div className="flex flex-col flex-1 pt-6">
+					<a rel="noopener noreferrer" href="#" aria-label="Te nulla oportere reprimique his dolorum"></a>
+					<a rel="noopener noreferrer" href="#" className="text-xs tracking-wider uppercase hover:underline font-nunito  dark:text-violet-600">AIR TICKETING</a>
+					
+					
+				</div>
+			</article>
+			<article className="flex flex-col dark:bg-gray-50">
+				<a rel="noopener noreferrer" href="#" aria-label="Te nulla oportere reprimique his dolorum">
+					<img alt="" className="object-cover w-full h-52 hover:scale-105 transition-all duration-1000 dark:bg-gray-500" src="https://images.pexels.com/photos/6328596/pexels-photo-6328596.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" />
+				</a>
+				<div className="flex flex-col flex-1 pt-6">
+					<a rel="noopener noreferrer" href="#" aria-label="Te nulla oportere reprimique his dolorum"></a>
+					<a rel="noopener noreferrer" href="#" className="text-xs tracking-wider uppercase hover:underline dark:text-violet-600 font-nunito"> VISA PROCESSING</a>
+				
+				
+				</div>
+			</article>
+			<article className="flex flex-col dark:bg-gray-50">
+				<a rel="noopener noreferrer" href="#" aria-label="Te nulla oportere reprimique his dolorum">
+					<img alt="" className="object-cover w-full h-52 hover:scale-105 transition-all duration-1000 dark:bg-gray-500" src="https://images.pexels.com/photos/823696/pexels-photo-823696.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" />
+				</a>
+				<div className="flex flex-col flex-1 pt-6">
+					<a rel="noopener noreferrer" href="#" aria-label="Te nulla oportere reprimique his dolorum"></a>
+					<a rel="noopener noreferrer" href="#" className="text-xs tracking-wider uppercase hover:underline font-nunito dark:text-violet-600">HOTEL BOOKING</a>
+				
+					
+				</div>
+			</article>
+			<article className="flex flex-col dark:bg-gray-50">
+				<a rel="noopener noreferrer" href="#" aria-label="Te nulla oportere reprimique his dolorum">
+					<img alt="" className="object-cover w-full h-52 hover:scale-105 transition-all duration-1000 dark:bg-gray-500" src="https://images.pexels.com/photos/16271983/pexels-photo-16271983/free-photo-of-ferris-wheel-in-black-and-white.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" />
+				</a>
+				<div className="flex flex-col flex-1 pt-6">
+					<a rel="noopener noreferrer" href="#" aria-label="Te nulla oportere reprimique his dolorum"></a>
+					<a rel="noopener noreferrer" href="#" className="text-xs tracking-wider uppercase hover:underline font-nunito dark:text-violet-600">HOLIDAY PACKAGE</a>
+				
+					
+				</div>
+			</article>
+		</div>
+	</div>
+</section></div>
+  )
+}
+
+export default OurServices
--- src/pages/Services/Services.jsx
+++ src/pages/Services/Services.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import OurServices from '../../components/Services/OurServices/OurServices'
+import PageHeader from '../../components/PageHeader/PageHeader'
 
 const Services = () => {
   return (
-    <div>Services</div>
+    <div>
+      <PageHeader heading={'Services'}  paragraph={'Our services are display'} bgImage={'https://images.pexels.com/photos/10967604/pexels-photo-10967604.png?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'}/>
+      <OurServices/>
+    </div>
   )
 }
 
